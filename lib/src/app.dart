22111a9c823ae729e090a8010d147d982fdf5bf9
Vicente Insfran <vinsfran@gmail.com>
2021-02-22 21:31:11 -0300

    Configuracion de rutas por nombres

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
 
 class App extends StatelessWidget {
   @override
@@ -8,6 +10,13 @@ class App extends StatelessWidget {
     return MaterialApp(
         title: 'Componentes App',
         debugShowCheckedModeBanner: false,
-        home: HomePage());
+        // home: HomePage()
+        initialRoute: '/',
+        routes: <String, WidgetBuilder>{
+          '/' : (BuildContext context) => HomePage(),
+          'alert' : (BuildContext context) => AlertPage(),
+          'avatar' : (BuildContext context) => AvatarPage(),
+        },
+        );
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -44,9 +44,9 @@ class HomePage extends StatelessWidget {
           color: Colors.blue,
         ),
         onTap: () {
-          final route = MaterialPageRoute(builder: (context) => AlertPage());
-
-          Navigator.push(context, route);
+          Navigator.pushNamed(context, opt['ruta']);
+          // final route = MaterialPageRoute(builder: (context) => AlertPage());
+          // Navigator.push(context, route);          
         },
       );
       opciones..add(widgetTemp)..add(Divider());
