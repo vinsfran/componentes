42217d0a0295ba778f9f538e3eac9f698b12fd24
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-02-23 09:35:04 -0300

    Image y FadeIn Image

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -9,7 +9,13 @@ class CardPage extends StatelessWidget {
       ),
       body: ListView(
         padding: EdgeInsets.all(10.0),
-        children: <Widget>[_cardTipo1()],
+        children: <Widget>[
+          _cardTipo1(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo2(),
+        ],
       ),
     );
   }
@@ -38,4 +44,24 @@ class CardPage extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget _cardTipo2() {
+  return Card(
+    child: Column(
+      children: <Widget>[
+        FadeInImage(
+            image: NetworkImage(
+                'https://static.photocdn.pt/images/articles/2017_1/iStock-545347988.jpg'),
+            placeholder: AssetImage('assets/images/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover),
+        // Image(
+        //     image: NetworkImage(
+        //         'https://static.photocdn.pt/images/articles/2017_1/iStock-545347988.jpg')),
+        Container(padding: EdgeInsets.all(10.0), child: Text('Texto'))
+      ],
+    ),
+  );
 }
