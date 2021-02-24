cc7a67d9b52acee35e364f5a34230e0032af565d
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-02-24 08:46:01 -0300

    79. inputFields part2

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -6,12 +6,46 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  String _nombre;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Inputs de texto'),
       ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        children: <Widget>[
+          _crearInput(),
+          Divider(),
+          _crearPersona(),
+        ],
+      ),
     );
+  }
+
+  Widget _crearInput() {
+    return TextField(
+      // autofocus: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          counter: Text('Letras ${_nombre.length}'),
+          hintText: 'Nombre de la persona',
+          labelText: 'Nombre',
+          helperText: 'Solo es el nombre',
+          suffix: Icon(Icons.accessibility),
+          icon: Icon(Icons.account_circle)),
+      onChanged: (valor) {
+        setState(() {
+          _nombre = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearPersona() {
+    return ListTile(title: Text('Nombre es: $_nombre'));
   }
 }
