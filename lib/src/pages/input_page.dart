c2069cf9781857fa1d9df5836c46e972f0556c2d
Vicente Insfran <vinsfra@gmail.com>
2021-02-23 22:47:17 -0300

    79. inputFields

---
+++ lib/src/pages/input_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inputs de texto'),
+      ),
+    );
+  }
+}
