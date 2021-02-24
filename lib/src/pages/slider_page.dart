48e602ef60acf6b1d07336a88c7a4de344ce5512
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-02-24 14:53:15 -0300

    84. Slide

---
+++ lib/src/pages/slider_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class SliderPage extends StatefulWidget {
+  @override
+  _SliderPageState createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+  double _valorSlider = 100.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Slider'),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 50.0),
+        child: Column(
+          children: <Widget>[
+            _crearSlider(),
+            Expanded(child: _crearImagen()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearSlider() {
+    return Slider(
+      activeColor: Colors.indigoAccent,
+      label: 'Tamaño de la imagen',
+      value: _valorSlider,
+      min: 10.0,
+      max: 400.0,
+      onChanged: (valor) {
+        setState(() {
+          _valorSlider = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearImagen() {
+    return FadeInImage(
+        image:
+            NetworkImage('http://pngimg.com/uploads/batman/batman_PNG111.png'),
+        placeholder: AssetImage('assets/images/jar-loading.gif'),
+        fadeInDuration: Duration(milliseconds: 200),
+        width: _valorSlider,
+        fit: BoxFit.contain);
+  }
+}
