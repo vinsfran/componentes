6a04e726809dc03167ebb693845dfd8703ee103a
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-02-25 11:26:30 -0300

    89. Pull to refresh

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -10,20 +10,20 @@ class ListViewPage extends StatefulWidget {
 class _ListViewPageState extends State<ListViewPage> {
   ScrollController _scrollController = new ScrollController();
 
-  List<int> _listaNumeros = [1, 2, 3, 4, 5];
-  int _cantidadCargar = 3;
+  List<int> _listaNumeros = new List();
   int _ultimoItem = 0;
   bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
-    _agregarLista(_cantidadCargar);
+    _agregar10();
+
     _scrollController.addListener(() {
       if (_scrollController.position.pixels ==
           _scrollController.position.maxScrollExtent) {
-        // _agregarLista(10);
-        _fechData();
+        // _agregar10();
+        fetchData();
       }
     });
   }
@@ -41,48 +41,63 @@ class _ListViewPageState extends State<ListViewPage> {
           title: Text('Listas'),
         ),
         body: Stack(
-          children: <Widget>[
-            _crearLoading(),
-            _crearLista(),
-          ],
+          children: <Widget>[_crearLista(), _crearLoading()],
         ));
   }
 
   Widget _crearLista() {
-    return ListView.builder(
-      controller: _scrollController,
-      itemCount: _listaNumeros.length,
-      itemBuilder: (BuildContext context, int index) {
-        final imagen = _listaNumeros[index];
+    return RefreshIndicator(
+      onRefresh: obtenerPagina1,
+      child: ListView.builder(
+        controller: _scrollController,
+        itemCount: _listaNumeros.length,
+        itemBuilder: (BuildContext context, int index) {
+          final imagen = _listaNumeros[index];
 
-        return FadeInImage(
-          image: NetworkImage('https://picsum.photos/500/300/?image=$imagen'),
-          placeholder: AssetImage('assets/images/jar-loading.gif'),
-        );
-      },
+          return FadeInImage(
+            image: NetworkImage('https://picsum.photos/500/300/?image=$imagen'),
+            placeholder: AssetImage('assets/images/jar-loading.gif'),
+          );
+        },
+      ),
     );
   }
 
-  void _agregarLista(int cantidad) {
-    for (int i = 1; i < cantidad; i++) {
+  Future<Null> obtenerPagina1() async {
+    final duration = new Duration(seconds: 2);
+    new Timer(duration, () {
+      _listaNumeros.clear();
+      _ultimoItem++;
+      _agregar10();
+    });
+
+    return Future.delayed(duration);
+  }
+
+  void _agregar10() {
+    for (var i = 1; i < 10; i++) {
       _ultimoItem++;
       _listaNumeros.add(_ultimoItem);
     }
+
     setState(() {});
   }
 
-  Future<Null> _fechData() async {
+  Future<Null> fetchData() async {
     _isLoading = true;
     setState(() {});
+
     final duration = new Duration(seconds: 2);
-    new Timer(duration, _respuestaHTTP);
+    return new Timer(duration, respuestaHTTP);
   }
 
-  void _respuestaHTTP() {
+  void respuestaHTTP() {
     _isLoading = false;
+
     _scrollController.animateTo(_scrollController.position.pixels + 100,
-        curve: Curves.fastOutSlowIn, duration: Duration(microseconds: 250));
-    _agregarLista(_cantidadCargar);
+        curve: Curves.fastOutSlowIn, duration: Duration(milliseconds: 250));
+
+    _agregar10();
   }
 
   Widget _crearLoading() {
