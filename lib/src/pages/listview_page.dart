720fe1f6859336c850e51ddaec4a846058a15e8c
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-02-25 09:41:51 -0300

    88. InfiniteScroll con Futures

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'dart:async';
+
 class ListViewPage extends StatefulWidget {
   @override
   _ListViewPageState createState() => _ListViewPageState();
@@ -9,28 +11,41 @@ class _ListViewPageState extends State<ListViewPage> {
   ScrollController _scrollController = new ScrollController();
 
   List<int> _listaNumeros = [1, 2, 3, 4, 5];
+  int _cantidadCargar = 3;
   int _ultimoItem = 0;
+  bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
-    _agregarLista(10);
+    _agregarLista(_cantidadCargar);
     _scrollController.addListener(() {
       if (_scrollController.position.pixels ==
           _scrollController.position.maxScrollExtent) {
-        _agregarLista(10);
+        // _agregarLista(10);
+        _fechData();
       }
     });
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Listas'),
-      ),
-      body: _crearLista(),
-    );
+        appBar: AppBar(
+          title: Text('Listas'),
+        ),
+        body: Stack(
+          children: <Widget>[
+            _crearLoading(),
+            _crearLista(),
+          ],
+        ));
   }
 
   Widget _crearLista() {
@@ -55,5 +70,37 @@ class _ListViewPageState extends State<ListViewPage> {
       _listaNumeros.add(_ultimoItem);
     }
     setState(() {});
+  }
+
+  Future<Null> _fechData() async {
+    _isLoading = true;
+    setState(() {});
+    final duration = new Duration(seconds: 2);
+    new Timer(duration, _respuestaHTTP);
+  }
+
+  void _respuestaHTTP() {
+    _isLoading = false;
+    _scrollController.animateTo(_scrollController.position.pixels + 100,
+        curve: Curves.fastOutSlowIn, duration: Duration(microseconds: 250));
+    _agregarLista(_cantidadCargar);
+  }
+
+  Widget _crearLoading() {
+    if (_isLoading) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[CircularProgressIndicator()],
+          ),
+          SizedBox(height: 15.0)
+        ],
+      );
+    } else {
+      return Container();
+    }
   }
 }
